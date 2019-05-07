7aa15430b800359adb9fa8e003ecc3189fc89740
David Acosta <davidacosta25081@yahoo.com>
2019-05-07 23:57:16 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,12 +1,12 @@
-class Dog 
+class Dog
 
-  attr_accessor :name, :breed, :age 
+  attr_accessor :name, :breed, :age
 
   def initialize(name,breed,age)
     @name = name
     @breed = breed
     @age = age
-  end 
+  end
 
-end 
+end
   
